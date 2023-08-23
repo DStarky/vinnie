44c0c47b8081b83fe44b7a39ce1f40235644d945
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-08-23 19:12:08 +0500

    first header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,13 @@ import NavBar from '../NavBar/NavBar';
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
   return (
     <header className={styles.Header}>
       <Link to="/" className={styles.Logo}>Винни-пух</Link>
       <NavBar />
+
     </header>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,22 @@
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.scss';
+import menuElements from '../../data/navigation';
+import { Phone } from 'lucide-react';
+
 
 const NavBar = () => {
   return (
     <nav>
       <ul className={styles.Navigation}>
-        <li><NavLink className={({ isActive }) => isActive ? styles.Active : ''} to="/">Главная</NavLink></li>
-        <li><NavLink className={({ isActive }) => isActive ? styles.Active : ''} to="delivery">Доставка</NavLink></li>
-        <li><NavLink className={({ isActive }) => isActive ? styles.Active : ''} to="about-us">О нас</NavLink></li>
-        <li><NavLink className={({ isActive }) => isActive ? styles.Active : ''} to="news">Новости</NavLink></li>
+        {menuElements.map(({ id, title, link }) => {
+          return <li key={id}>
+            <NavLink className={({ isActive }) => isActive ? styles.Active : styles.Link} to={link}>{title}</NavLink>
+          </li>
+        })}
+        <div className={styles.Phone}>
+          <Phone color='hsl(328, 92%, 47%)' />
+          <a href="tel:+73532307536">+7 (3532) 30-75-36</a>
+        </div>
       </ul>
     </nav>
   )
