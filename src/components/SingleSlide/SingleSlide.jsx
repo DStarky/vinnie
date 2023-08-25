ab2fa0b41116ab69adf71e110cca6085749a55c5
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-08-25 13:06:47 +0500

    remade single slide

--- src/components/SingleSlide/SingleSlide.jsx
+++ src/components/SingleSlide/SingleSlide.jsx
@@ -2,21 +2,29 @@ import { useMediaQuery } from 'react-responsive';
 
 import Button from "../Button/Button";
 import styles from "./SingleSlide.module.scss";
+import { Link } from 'react-router-dom';
 
 const SingleSlide = () => {
   const isMoreThan769 = useMediaQuery({ minWidth: 769 })
 
   return (
     <div className={styles.SingleSlide}>
-      <div className={isMoreThan769 ? styles.Content : styles.ContentAbsolute}>
-        <h1 className={styles.Title}>Бесплатная доставка</h1>
-        <p className={styles.Text}>Только до конца месяца</p>
-        <Button text="Перейти к новости" />
-      </div>
+      {isMoreThan769 && <div className={styles.Content}>
+        <h1 className={styles.Title} >Бесплатная доставка</h1>
+        <p className={styles.Text}>Только до  конца месяца</p>
+        <Link to='delivery'><Button text="Перейти к новости" /></Link>
+      </div>}
       <div className={styles.Cover}>
-        <img src="images/slider-1.jpg" className={styles.Image} />
+        {
+          isMoreThan769
+            ? <img src="images/slider-1.jpg" className={styles.Image} />
+            : <Link to='delivery'>
+              <img src="images/slider-1-small.jpg" className={styles.Image} />
+            </Link>
+        }
       </div>
     </div>
   )
 }
 export default SingleSlide
+
