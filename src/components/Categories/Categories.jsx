91e3173c5275eee09c3b38bd00f1a63cf7d60d9c
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-08-28 16:12:14 +0500

    fix style for categories component

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,18 +1,28 @@
+import { useMediaQuery } from 'react-responsive';
 import styles from './Categories.module.scss';
-
-
 
 const Categories = (props) => {
   const { categories, activeIndex, setActiveIndex } = props;
 
+  const isMoreThan769 = useMediaQuery({ minWidth: 769 });
+
   return (
-    <ul className={styles.Parent}>
+    <ul
+      className={styles.Parent}
+      style={isMoreThan769 ? { margin: '2.4rem 0' } : { margin: '1.2rem' }}>
       {categories.map((category, index) => {
-
-        return <li key={index} className={`${styles.Category} ${activeIndex === index ? styles.ActiveCategory : ''}`} onClick={() => setActiveIndex(index)}>{category}</li>
-
+        return (
+          <li
+            key={index}
+            className={`${styles.Category} ${
+              activeIndex === index ? styles.ActiveCategory : ''
+            }`}
+            onClick={() => setActiveIndex(index)}>
+            {category}
+          </li>
+        );
       })}
     </ul>
-  )
-}
-export default Categories
+  );
+};
+export default Categories;
