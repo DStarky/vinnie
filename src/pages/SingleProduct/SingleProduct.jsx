3f6566972a8acbd3a057981060fc199a721069ca
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-08-29 11:37:09 +0500

    first structure for single page product

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -6,7 +6,6 @@ import styles from './SingleProduct.module.scss';
 const SingleProduct = () => {
   const { slug } = useParams();
   const [cake, setCake] = useState({});
-  console.log(cake);
   useEffect(() => {
     fetch(
       `https://64e5c4a909e64530d17efcf9.mockapi.io/productions?slug=${slug}`,
@@ -18,21 +17,31 @@ const SingleProduct = () => {
 
   return (
     <>
-      {cake ? (
+      {cake && (
         <article className={styles.Page}>
           <div className={styles.Cover}>
             <img src={cake.image} />
           </div>
           <div className={styles.Content}>
             <ul className={styles.Properties}>
-              {cake.properties.map((property, index) => {
-                <li key={index}>{property}</li>;
+              {cake.properties?.map((property, index) => {
+                return (
+                  <li
+                    className={styles.Property}
+                    key={index}>
+                    {property}
+                  </li>
+                );
               })}
             </ul>
+            <h2 className={styles.Nmae}>{cake.name}</h2>
+            <p className={styles.Weight}>Вес: {cake.weight}</p>
+            <p className={styles.Description}>{cake.description}</p>
+            <p className={styles.Price}>
+              <span>{cake.price}</span> руб.
+            </p>
           </div>
         </article>
-      ) : (
-        <h1>Page Not Found</h1>
       )}
     </>
   );
