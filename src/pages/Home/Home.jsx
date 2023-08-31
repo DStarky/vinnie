aa6e47d03cd47a7ed58206e2991a6c6067a69ee7
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-08-31 07:02:06 +0500

    home render categories

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,13 +8,12 @@ import categories from '../../data/categories.json';
 const Home = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [cakes, setCakes] = useState([]);
-
   useEffect(() => {
-    fetch('https://64e5c4a909e64530d17efcf9.mockapi.io/productions')
+    fetch(`https://64e5c4a909e64530d17efcf9.mockapi.io/productions${categories[activeIndex].request}`)
       .then((data) => data.json())
       .then((cakes) => setCakes(cakes))
       .catch((e) => console.log(e));
-  }, []);
+  }, [activeIndex]);
 
   return (
     <>
