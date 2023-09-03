11ffa08936509b11bab9458f4aa476069babd514
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-03 13:08:42 +0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
+import NavBar from '../NavBar/NavBar';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    <div className={styles.Footer}>
+      <Link
+        to='/'
+        className={styles.Logo}>
+        Винни-пух
+      </Link>
+      <div className={styles.Navigation}>
+        <NavBar />
+      </div>
+      <div className={styles.Checkout}>123</div>
+      <div className={styles.Worktime}>321</div>
+      <div className={styles.Social}>231</div>
+    </div>
+  );
+};
+export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,29 +14,58 @@ import styles from './Header.module.scss';
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const isMoreThan769 = useMediaQuery({ minWidth: 769 })
-  const isLessThan768 = useMediaQuery({ maxWidth: 768 })
-  const isLessThan993 = useMediaQuery({ maxWidth: 993 })
-  const isLessThan1100 = useMediaQuery({ maxWidth: 1100 })
-  const isLessThan1200 = useMediaQuery({ maxWidth: 1200 })
+  const isMoreThan769 = useMediaQuery({ minWidth: 769 });
+  const isLessThan768 = useMediaQuery({ maxWidth: 768 });
+  const isLessThan993 = useMediaQuery({ maxWidth: 993 });
+  const isLessThan1100 = useMediaQuery({ maxWidth: 1100 });
+  const isLessThan1200 = useMediaQuery({ maxWidth: 1200 });
 
   return (
-    <header className={styles.Header} style={isLessThan768 ? { borderRadius: 0, marginBottom: 0 + 'rem' } : { marginBottom: 2 + 'rem' }}>
-      <Link to="/" className={styles.Logo} style={isLessThan1200 ? isLessThan993 ? { fontSize: 2.4 + 'rem', marginRight: 1 + 'rem' } : { marginRight: 2.8 + 'rem' } : {}}>
+    <header
+      className={styles.Header}
+      style={
+        isLessThan768
+          ? { borderRadius: 0, marginBottom: 0 + 'rem' }
+          : { marginBottom: 2 + 'rem' }
+      }>
+      <Link
+        to='/'
+        className={styles.Logo}
+        style={
+          isLessThan1200
+            ? isLessThan993
+              ? { fontSize: 2.4 + 'rem', marginRight: 1 + 'rem' }
+              : { marginRight: 2.8 + 'rem' }
+            : {}
+        }>
         Винни-пух
       </Link>
-      {<NavBar isOpen={isMenuOpen} setOpen={setIsMenuOpen} isShow={isLessThan1100} />}
-      <div className={styles.Buttons} >
-        {isMoreThan769 && <>
-          <NotificationButton notifications={2} />
-          <Link to="favourites"><FavoriteButton /></Link>
-          <Link to="user-page"><UserButton /></Link>
-        </>}
+      {
+        <NavBar
+          isOpen={isMenuOpen}
+          setOpen={setIsMenuOpen}
+          isShow={isLessThan1100}
+        />
+      }
+      <div className={styles.Buttons}>
+        {isMoreThan769 && (
+          <>
+            <NotificationButton notifications={2} />
+            <Link to='favourites'>
+              <FavoriteButton />
+            </Link>
+            <Link to='user-page'>
+              <UserButton />
+            </Link>
+          </>
+        )}
 
-        <Link to="basket"><BasketButton showText={isMoreThan769} /></Link>
+        <Link to='basket'>
+          <BasketButton showText={isMoreThan769} />
+        </Link>
         {isLessThan1100 && <BurgerButton setOpen={setIsMenuOpen} />}
       </div>
-    </header >
-  )
-}
-export default Header
+    </header>
+  );
+};
+export default Header;
