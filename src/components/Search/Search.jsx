8c1cd717e53947810a0899af28dc067e2abc4679
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-09 09:34:07 +0500

    add debounce for search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,16 +2,31 @@ import { useDispatch } from 'react-redux';
 import { setSearchValue } from '../../redux/slices/filterSlice';
 import debounce from 'lodash.debounce';
 import styles from './Search.module.scss';
+import { useCallback } from 'react';
+import { useState } from 'react';
 
 const Search = ({ searchValue }) => {
   const dispatch = useDispatch();
+  const [value, setValue] = useState('');
+
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      dispatch(setSearchValue({ text: str }));
+    }, 250),
+    [],
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={styles.Root}>
       <input
         className={styles.Input}
-        value={searchValue}
-        onChange={(e) => dispatch(setSearchValue({ text: e.target.value }))}
+        value={value}
+        onChange={onChangeInput}
         placeholder='Найти вкусняшку ...'
       />
     </div>
