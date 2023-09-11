fef01f71ca2a733cd4d9a5f49d621820b9a752f1
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-11 07:48:47 +0500

    add component for basket

---
+++ src/components/BasketProduct/BasketProduct.jsx
@@ -0,0 +1,8 @@
+import styles from './BasketProduct.module.scss';
+
+const BasketProduct = () => {
+  return (
+    <div>BasketProduct</div>
+  )
+}
+export default BasketProduct
--- src/pages/BasketPage/BasketPage.jsx
+++ src/pages/BasketPage/BasketPage.jsx
@@ -1,16 +1,22 @@
+import BasketProduct from '../../components/BasketProduct/BasketProduct';
 import styles from './BasketPage.module.scss';
 
 const BasketPage = () => {
-  return <section className={styles.Basket}>
-    <h2>Корзина</h2>
+  return (
+    <section className={styles.Basket}>
+      <h2>Корзина</h2>
 
-    <main className={styles.Content}>
-      {/* <p className={styles.NothingText}>Вы пока ничего не добавили :(</p> */}
-      <div className={styles.Titles}>
-        <h3>Все товары</h3>
-        <button className={styles.ClearBasketButton}>Очистить корзину</button>
-      </div>
-    </main>
-  </section>;
+      <main className={styles.Content}>
+        {/* <p className={styles.NothingText}>Вы пока ничего не добавили :(</p> */}
+        <div className={styles.Titles}>
+          <h3>Все товары</h3>
+          <button className={styles.ClearBasketButton}>Очистить корзину</button>
+        </div>
+        <ul className={styles.List}>
+          <BasketProduct />
+        </ul>
+      </main>
+    </section>
+  );
 };
 export default BasketPage;
