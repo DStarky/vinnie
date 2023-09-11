ad56282124ad4c48d1eef9a42ba8388786b21ec6
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-11 07:44:19 +0500

    basket first version

--- src/pages/BasketPage/BasketPage.jsx
+++ src/pages/BasketPage/BasketPage.jsx
@@ -1,6 +1,16 @@
+import styles from './BasketPage.module.scss';
+
 const BasketPage = () => {
-  return (
-    <h1>BasketPage</h1>
-  )
-}
-export default BasketPage
+  return <section className={styles.Basket}>
+    <h2>Корзина</h2>
+
+    <main className={styles.Content}>
+      {/* <p className={styles.NothingText}>Вы пока ничего не добавили :(</p> */}
+      <div className={styles.Titles}>
+        <h3>Все товары</h3>
+        <button className={styles.ClearBasketButton}>Очистить корзину</button>
+      </div>
+    </main>
+  </section>;
+};
+export default BasketPage;
