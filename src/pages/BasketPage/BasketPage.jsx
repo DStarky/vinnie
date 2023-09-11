8f8e8d437ddde731fddbb1f96f31d34e27814a57
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-11 08:42:06 +0500

    add conditional rendering for basket page

--- src/pages/BasketPage/BasketPage.jsx
+++ src/pages/BasketPage/BasketPage.jsx
@@ -1,20 +1,32 @@
+import { useSelector } from 'react-redux';
 import BasketProduct from '../../components/BasketProduct/BasketProduct';
 import styles from './BasketPage.module.scss';
 
 const BasketPage = () => {
+  const basket = useSelector((state) => state.basket.products);
+
   return (
     <section className={styles.Basket}>
       <h2>Корзина</h2>
 
       <main className={styles.Content}>
-        {/* <p className={styles.NothingText}>Вы пока ничего не добавили :(</p> */}
-        <div className={styles.Titles}>
-          <h3>Все товары</h3>
-          <button className={styles.ClearBasketButton}>Очистить корзину</button>
-        </div>
-        <ul className={styles.List}>
-          <BasketProduct />
-        </ul>
+        {basket.length ? (
+          <>
+            <div className={styles.Titles}>
+              <h3>Все товары</h3>
+              <button className={styles.ClearBasketButton}>
+                Очистить корзину
+              </button>
+            </div>
+            <ul className={styles.List}>
+              {basket.map((el) => {
+                return <BasketProduct key={el.name} />;
+              })}
+            </ul>
+          </>
+        ) : (
+          <p className={styles.NothingText}>Вы пока ничего не добавили :(</p>
+        )}
       </main>
     </section>
   );
