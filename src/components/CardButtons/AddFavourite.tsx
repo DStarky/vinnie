357e5d7171ec39471f6dda87457246bce13bbad3
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-15 10:40:55 +0500

    add favourite for ts

--- src/components/CardButtons/AddFavourite.tsx
+++ src/components/CardButtons/AddFavourite.tsx
@@ -2,15 +2,14 @@ import { LuHeart } from 'react-icons/lu';
 import styles from './CardButtons.module.scss';
 import { useState } from 'react';
 
-const AddFavourite = () => {
+const AddFavourite: React.FC = () => {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <button
-    className={`${styles.Favourite} ${isActive ? styles.Active : ''}`}
+      className={`${styles.Favourite} ${isActive ? styles.Active : ''}`}
       onClick={() => setIsActive((prev) => !prev)}>
-      
-      <LuHeart size='24px'/>
+      <LuHeart size='24px' />
     </button>
   );
 };
