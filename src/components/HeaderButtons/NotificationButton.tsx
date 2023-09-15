22bc2601b77720c649a98576d63206a1785777e2
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-15 10:14:41 +0500

    Notification refuck to typesrcipt

--- src/components/HeaderButtons/NotificationButton.tsx
+++ src/components/HeaderButtons/NotificationButton.tsx
@@ -4,7 +4,7 @@ import Modal from '../Modal/Modal';
 import styles from './Buttons.module.scss';
 import notifications from '../../data/notifications.json';
 
-const NotificationButton = () => {
+const NotificationButton: React.FC = () => {
   const [notificationsCount, setNotificationsCount] = useState(
     notifications.length,
   );
@@ -13,14 +13,16 @@ const NotificationButton = () => {
   const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
 
   const modalRef = useRef(null);
-  const buttonRef = useRef(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     // Устанавливаем начальную позицию модального окна
-    const buttonRect = buttonRef.current.getBoundingClientRect();
-    const modalLeft = buttonRect.left;
-    const modalTop = buttonRect.top + buttonRect.height;
-    setModalPosition({ top: modalTop + 20, left: modalLeft });
+    if (buttonRef.current) {
+      const buttonRect = buttonRef.current.getBoundingClientRect();
+      const modalLeft = buttonRect.left;
+      const modalTop = buttonRect.top + buttonRect.height;
+      setModalPosition({ top: modalTop + 20, left: modalLeft });
+    }
   }, []);
 
   function showModal() {
