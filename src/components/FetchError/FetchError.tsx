0f1ce0375c3ba5b96f9a0d5e7476e3a5040b1758
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-09-15 11:11:09 +0500

    fetch error component to type script

--- src/components/FetchError/FetchError.tsx
+++ src/components/FetchError/FetchError.tsx
@@ -1,6 +1,6 @@
 import styles from './FetchError.module.scss';
 
-const FetchError = () => {
+const FetchError: React.FC = () => {
   return <div>Произошла ошибка :(</div>;
 };
 export default FetchError;
